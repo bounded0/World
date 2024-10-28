818debecb66742e17916c7a4e0358cedb062e121
bounded0 <erenunaleng@gmail.com>
2024-10-29 00:33:19 +0300

    Updated

--- app.js
+++ app.js
@@ -1,51 +1,39 @@
-import axios from "axios"
-import express from "express"
-import bodyParser from "body-parser"
+import axios from "axios";
+import express from "express";
+import bodyParser from "body-parser";
 
-let countryNames = []
 
-const app = express()
+const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
-app.use(express.static('public'))
-app.get("/",(req,res)=>{
-    res.render("index.ejs")
-})
-app.get("/:continentName", async (req,res)=>{
-    let {continentName} = req.params
-    console.log(continentName + " asa")
-    if(continentName.includes("north")){
-        continentName = "North America"
-    }
-    if(continentName.includes("south")){
-        continentName = "South America"
-    }
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
 
-    const result = await axios.get("https://restcountries.com/v3.1/all")
+let countryName = [];
 
-    result.data.forEach(country => {
-       
-        if(country.continents[0] ===`${continentName}`){
-            console.log(country.name.common, "=>", country.continents[0]);
-            countryNames.push(country.name.common)
-         
-        }
+app.get("/:continentName", async (req, res) => {
+  let { continentName } = req.params;
+  const formattedName = formattedContinentName(continentName);
+  const viewName = formattedName.toLowerCase().replace(" ", "-");
+  const result = await axios.get("https://restcountries.com/v3.1/all");
 
-      });
+  const countryNames = result.data
+    .filter((country) => country.continents[0] === formattedName)
+    .map((country) => ({name : country.name.common, capital : country.capital, flag : country.flags.png}));
+  res.render(`${viewName}.ejs`, { continentName: formattedName, countryNames });
+  countryName = [];
+});
 
-    if(continentName.includes(continentName,"north")){
-     continentName = "north-america"
-     console.log( continentName,"sadaad")
-    }
-    if(continentName.includes(continentName,"south")){
-        continentName = "south-america"
-        console.log( continentName,"sadaad")
-       }
-    res.render(`${continentName}.ejs`, { continentName,countryNames});
-    countryNames = []
 
-})
+function formattedContinentName(name) {
+  const normalized = name.toLowerCase();
+  if (normalized.includes("north")) return "North America";
+  if (normalized.includes("south")) return "South America";
+  return name;
+}
 
-app.listen(3000,()=>{
-    console.log("Working..")
-})
+app.listen(3000, () => {
+  console.log("Working..");
+});
